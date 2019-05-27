0b1992b2f76d73f0f4a2ee29697b006f14181332
Jung, Jinsol <smokerjs.dev@gmail.com>
2019-05-27 16:39:56 +0900

    day01 - study08 operator multilpy

---
+++ day01/study08.php
@@ -0,0 +1,13 @@
+<?
+  // 연산자 - 곱하기 연산
+  $personnel = 3;
+  $price = 7000;
+  $result = $personnel * $price;
+  echo "인원 수 : $personnel<br/>";
+  echo "가격 : $price<br/>";
+  echo "총액 : $result<br/>";
+  /*
+    1. 곱하기 연산은 *을 사용한다.
+  */
+?>
+
