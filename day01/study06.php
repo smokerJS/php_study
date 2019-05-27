3d92a5f25182b0f4a1feb483d8a68270252e8bb4
Jung, Jinsol <smokerjs.dev@gmail.com>
2019-05-27 16:28:55 +0900

    day01 - study06 operator minus

---
+++ day01/study06.php
@@ -0,0 +1,13 @@
+<?
+  // 연산자 - 빼기 연산하기
+  $money = 10000;
+  $price = 6000;
+  $result = $money - $price;
+  echo "가진 돈 : $money<br/>";
+  echo "가격 : $price<br/>";
+  echo "남은 돈 : $result<br/>";
+  /*
+    1. 숫자의 빼기 연산은 - 를 사용한다.
+  */
+?>
+
