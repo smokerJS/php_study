bf93057796d86dd74821697d33368c162629c47d
Jung, Jinsol <smokerjs.dev@gmail.com>
2019-05-27 15:25:41 +0900

    da01 - study01 add remark

--- day01/lec01.php
+++ day01/lec01.php
@@ -1,4 +1,11 @@
 <?
   echo "<p>Hello PHP</p>";  // 출력문 echo "HTML 태그 사용가능"
   echo "<strong>PHP Start</strong>";
+  /*
+    1. PHP 시작태그 <?  종료태그 ?>
+    2. echo문은 문자열을 브라우저에 출력한다.
+    3. 이 때 HTML태그를 인식한다.
+    4. PHP는 모든 구문의 끝에 ;(세미콜론)을 붙여야한다.
+  */
 ?>
+
