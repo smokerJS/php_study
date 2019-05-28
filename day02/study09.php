ca1eb9ca3c03accae4b4933df207045263bd83ef
Jung, Jinsol <smokerjs.dev@gmail.com>
2019-05-28 13:44:50 +0900

    day02 - study09 loop multiple while

---
+++ day02/study09.php
@@ -0,0 +1,15 @@
+<?
+  $rand = 13;
+  while($rand) {
+    echo $rand--;
+    echo "<br/>";
+  }
+  /*
+    반복문 while
+    while(조건) {
+      명령문
+    }
+    for loop는 반복횟수가 명확할 때 사용하는 반면, while loop는 반복횟수가 명확하지 않을 때 사용한다.
+    조건이 false가 되면 반복문을 탈출한다.
+  */
+?>
