5c0193c39a9cd6c6eebe5dcccb6b247622a65fa1
Jung, Jinsol <smokerjs.dev@gmail.com>
2019-05-29 13:51:01 +0900

    day03 - exam01

---
+++ day03/exam01.php
@@ -0,0 +1,33 @@
+<?
+  echo "
+    <style>
+      table,th,td {
+        text-align: center;
+        border: 1px solid #000;
+        border-collapse: collapse;
+      }
+      th {
+        width: 200px;
+        padding: 10px 0;
+      }
+    </style>
+  ";
+  echo "<table>";
+    for($i = 2; $i <= 9; $i++) {
+      echo "<tr>";
+      echo "<th colspan='3'>".$i++."단</th>";
+      echo "<th colspan='3'>".$i."단</th>";
+      echo "</tr>";
+      for($j = 1; $j < 10; $j+=3) {
+        echo "<tr>";
+        for($k = 0; $k < 6; $k++) {
+          $num1 = $i-1;
+          $num2 = $k+$j;
+          $k >= 3 && $num1++ && $num2 -= 3;
+          echo "<td>$num1 * $num2 = ".$num1*$num2."</td>";
+        }
+        echo "</tr>";
+      }
+    }
+  echo "</table>";
+?>
