51879aff548062a656a301a693173aa076ad4016
Jung, Jinsol <smokerjs.dev@gmail.com>
2019-05-29 13:50:33 +0900

    day03 - question

---
+++ day03/question.php
@@ -0,0 +1,13 @@
+<?
+  echo "<h1>반복문을 이용하여 아래 이미지와 같이 구구단을 표현</h1>";
+  echo "<p>(HTML 태그의 table 태그를 이용하여 출력)</p>";
+  echo "<img src='/images/img-day03-question01.png'";
+  echo "<br/><br/><br/><br/><br/>";
+  echo "<h1>반복문을 이용하여 아래 이미지와 같이 구구단을 표현 (3단, 6단, 9단)</h1>";
+  echo "<p>(HTML 태그의 table 태그를 이용하여 출력)</p>";
+  echo "<img src='/images/img-day03-question02.png'";
+  echo "<br/><br/><br/><br/><br/>";
+  echo "<h1>반복문을 사용하여 아래와 같은 형태로 출력하는 프로그램을 작성</h1>";
+  echo "<img src='/images/img-day03-question03.png'";
+  echo "<br/><br/><br/><br/><br/>";
+?>
