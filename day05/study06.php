a6df7ff312796a11f68320fe6fcaf4793daeb0f4
Jung, Jinsol <smokerjs.dev@gmail.com>
2019-06-10 11:24:39 +0900

    day05 - study06 function return

---
+++ day05/study06.php
@@ -0,0 +1,10 @@
+<?
+  function customPlus($a, $b) {
+    return $a+$b;
+  }
+  echo(customPlus(36,64));
+  /*
+    함수 선언시 함수명 옆의 ()괄호에 함수에서 사용할 매개변수를 지정할 수 있다.
+    return 구문으로 반환값을 정의할 수 있다.
+  */
+?>
