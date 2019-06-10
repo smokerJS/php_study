4fbdfc1f38a133bea1d99c26e7ea9e937c365be4
Jung, Jinsol <smokerjs.dev@gmail.com>
2019-06-10 11:21:38 +0900

    day05 - study05 default function - mysql

---
+++ day05/study05.php
@@ -0,0 +1,10 @@
+<?
+  // PHP의 내장함수
+
+  // MySQL 관련 내장함수
+
+  mysql_connect(); // 데이터베이스 접속
+  mysql_create_db(); // 데이터베이스 생성
+  mysql_drop_db(); // 데이터베이스 삭제
+  mysql_clode(); // 데이터베이스 접속 종료
+?>
